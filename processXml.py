e6e5e8fdae63021dc7024bde4873409d484cc0fc
olawolemoses@gmail.com <a2017@myhost.local>
2017-11-14 17:10:01 +0100

    added sleep variables

--- processXml.py
+++ processXml.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import random as random
+import time
 
 infile = open("srcpage1.xml","r")
 contents = infile.read()
@@ -15,11 +17,31 @@ for story in storylist_latest:
     # print story.a.get_text()
     urls.append("http://www.eurosport.com" + story.a['href'])
 print urls
-    
+# 
+
+record_i = 0
+
 for url in urls:
     page = requests.get( url )
     soup = BeautifulSoup(page.text, 'html.parser')
     html = soup.findAll("div", class_='storyfull__container')
     print html
     print "=" * 50
+    if record_i % 50 == 0:
+        ticker = random.randrange(300, 400, 2)
+        print "sleeping for %s" % (ticker)
+        time.sleep(ticker)
+    elif record_i % 20 == 0:
+        ticker = random.randrange(180, 300, 2)
+        print "sleeping for %s" % (ticker)
+        time.sleep(ticker)
+    elif record_i % 4 == 0:
+        ticker = random.randrange(60, 121, 2)
+        print "sleeping for %s" % (ticker)
+        time.sleep(ticker)
+    else:
+        ticker = random.randrange(25, 50, 2)
+        print "sleeping for %s" % (ticker)
+        time.sleep(ticker)
+        
 #     t = (id,GSID,cell.text,cell['href'])
